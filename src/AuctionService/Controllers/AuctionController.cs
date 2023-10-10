d7ceb89974411ef578f4b4384e14429d97c67c4c
CallMeYurii <vad4807@gmail.com>
2023-10-10 13:58:59 +0300

    Add rabbitmq and make asynchronous communication between services during creation of item

--- src/AuctionService/Controllers/AuctionController.cs
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -3,6 +3,8 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,11 +16,16 @@ public class AuctionController : ControllerBase
 {
     private readonly AuctionDbContext _context;
     private readonly IMapper _mapper;
+    private readonly IPublishEndpoint _publishEndpoint;
 
-    public AuctionController(AuctionDbContext context, IMapper mapper)
+    public AuctionController(
+        AuctionDbContext context, 
+        IMapper mapper,
+        IPublishEndpoint publishEndpoint)
     {
         _context = context;
         _mapper = mapper;
+        _publishEndpoint = publishEndpoint;
     }
 
     [HttpGet]
@@ -56,9 +63,19 @@ public class AuctionController : ControllerBase
         auction.Seller = "test_user";
 
         _context.Auctions.Add(auction);
+        
+        var newAuction = _mapper.Map<AuctionDto>(auction);
+
+        await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
+        
         var result = await _context.SaveChangesAsync() > 0;
-        if (result) return CreatedAtAction(nameof(GetAuctionById), new {auction.Id}, _mapper.Map<AuctionDto>(auction));
-        return BadRequest("Could not save changes to db!");
+
+
+        
+        if (!result) return BadRequest("Could not save changes to db!");
+        
+        return CreatedAtAction(nameof(GetAuctionById), 
+            new {auction.Id}, newAuction);
     }
 
     [HttpPut("{id}")]
