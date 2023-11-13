f806b597220b0a2cad5f1b578eff8c1f85049291
CallMeYurii <vad4807@gmail.com>
2023-11-13 19:41:18 +0200

    Add Gateway Service

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,34 @@
+using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers
+{
+    public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+    {
+        private readonly AuctionDbContext _context;
+
+        public AuctionFinishedConsumer(AuctionDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+        {
+            Console.WriteLine("---> Consuming auction finished");
+
+            var auction = await _context.Auctions.FindAsync(context.Message.AuctionId);
+
+            if (context.Message.ItemSold)
+            {
+                auction.Winner = context.Message.Winner;
+                auction.SoldAmount = context.Message.Amount;
+            }
+
+            auction.Status = auction.SoldAmount > auction.ReservePrice
+                ? Entities.Status.Finished : Entities.Status.ReserveNotMet;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
